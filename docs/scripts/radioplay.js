b7fa4f415b32b0d752ef0115633b799f12801909
Stephen L <67349718+StephenL13@users.noreply.github.com>
2023-04-23 15:23:54 +0800

    final

--- docs/scripts/radioplay.js
+++ docs/scripts/radioplay.js
@@ -1,6 +1,7 @@
 var audio = new Audio('https://eu8.fastcast4u.com/proxy/stephenl13?mp=/1');
 var playstopBtn = document.querySelector('.playstop');
 var volumeCtrl = document.querySelector('#volume-control');
+var volumeIcon = document.querySelector('.volume-speaker');
 var songinfo = document.getElementsByClassName("songinfo")[0];
 var loadingIcon = document.querySelector('.status-text');
 let slowInternetTimeout = null;
@@ -12,7 +13,7 @@ window.addEventListener('load', () => {
 
 audio.addEventListener('waiting', () => {
 slowInternetTimeout = setTimeout(() => {
-    playstopBtn.innerHTML = '<i class="fa-solid fa-spinner fa-spin-pulse"></i>';
+    playstopBtn.innerHTML = '<i class="fa-solid fa-spinner fa-3x fa-spin-pulse"></i>';
     });
 });
 
@@ -23,27 +24,27 @@ if(slowInternetTimeout != null){
     //continue playing
     loadingIcon.innerHTML = "";
     songinfo.style.display = "block";
-    playstopBtn.innerHTML = '<i class="fa-solid fa-circle-pause" onclick="playstop()"></i>';
+    playstopBtn.innerHTML = '<i class="fa-solid fa-circle-pause fa-3x" onclick="playstop()"></i>';
     }
 });
 
 audio.addEventListener('ended', () => {
     loadingIcon.innerHTML = '<i class="fa-solid fa-spinner fa-triangle-exclamation"></i> DZCP is now signing off. Thank you for listening.'
-    playstopBtn.innerHTML = '<i class="fa-solid fa-circle-play" onclick="playstop()"></i>';
+    playstopBtn.innerHTML = '<i class="fa-solid fa-circle-play fa-3x" onclick="playstop()"></i>';
 });
 
 audio.addEventListener('error', () => {
     if(audio.error.code === 4) {
         loadingIcon.innerHTML = '<i class="fa-solid fa-spinner fa-triangle-exclamation"></i> The station is offline. Come back soon.'
-        playstopBtn.innerHTML = '<i class="fa-solid fa-circle-play" onclick="playstop()"></i>';
+        playstopBtn.innerHTML = '<i class="fa-solid fa-circle-play fa-3x" onclick="playstop()"></i>';
     } else if (audio.error.code === 3){
         audio.pause();
         loadingIcon.innerHTML = '<i class="fa-solid fa-spinner fa-triangle-exclamation"></i> A decoding error has occurred. Refresh your browser.'
-        playstopBtn.innerHTML = '<i class="fa-solid fa-circle-play" onclick="playstop()"></i>';
+        playstopBtn.innerHTML = '<i class="fa-solid fa-circle-play fa-3x" onclick="playstop()"></i>';
     } else if (audio.error.code === 2){
         audio.pause();
         loadingIcon.innerHTML = '<i class="fa-solid fa-spinner fa-triangle-exclamation"></i> A network error has occurred. Please try again.'
-        playstopBtn.innerHTML = '<i class="fa-solid fa-circle-play" onclick="playstop()"></i>';
+        playstopBtn.innerHTML = '<i class="fa-solid fa-circle-play fa-3x" onclick="playstop()"></i>';
     }
 })
 
@@ -55,14 +56,31 @@ function playstop() {
         } else {
             audio.pause();
             loadingIcon.innerHTML = '';
-            playstopBtn.innerHTML = '<i class="fa-solid fa-circle-play" onclick="playstop()"></i>';
+            playstopBtn.innerHTML = '<i class="fa-solid fa-circle-play fa-3x" onclick="playstop()"></i>';
         }
     } catch (err) {
         loadingIcon.innerHTML = '<i class="fa-solid fa-spinner fa-triangle-exclamation"></i> An error has occured.'
-        playstopBtn.innerHTML = '<i class="fa-solid fa-circle-play" onclick="playstop()"></i>';
+        playstopBtn.innerHTML = '<i class="fa-solid fa-circle-play fa-3x" onclick="playstop()"></i>';
     }
 }
 
 function setVolume() {
     audio.volume = volumeCtrl.value / 100;
+    if(audio.volume < 0.01) {
+        volumeIcon.innerHTML = '<i class="fa-solid fa-volume-mute"></i>'
+    } else {
+        volumeIcon.innerHTML = '<i class="fa-solid fa-volume-up"></i>'
+    }
+}
+
+function speakerIcon() {
+    if(audio.volume <= 0) {
+        volumeIcon.innerHTML = '<i class="fa-solid fa-volume-up"></i>'
+        volumeCtrl.value = 100
+        audio.volume = 1
+    } else if (audio.volume == 100 || audio.volume <= 99) {
+        volumeIcon.innerHTML = '<i class="fa-solid fa-volume-mute"></i>'
+        volumeCtrl.value = 0
+        audio.volume = 0
+    }
 }
