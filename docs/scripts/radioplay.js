ba974e2ff741474bcd8354a98062e0a41bb48c5b
Stephen L <67349718+StephenL13@users.noreply.github.com>
2023-04-23 14:58:01 +0800

    new audio controls

--- docs/scripts/radioplay.js
+++ docs/scripts/radioplay.js
@@ -1,19 +1,18 @@
 var audio = new Audio('https://eu8.fastcast4u.com/proxy/stephenl13?mp=/1');
-var playstopBtn = document.querySelector('#playstop');
+var playstopBtn = document.querySelector('.playstop');
 var volumeCtrl = document.querySelector('#volume-control');
-var loadingIcon = document.querySelector('.loading-icon');
+var songinfo = document.getElementsByClassName("songinfo")[0];
+var loadingIcon = document.querySelector('.status-text');
 let slowInternetTimeout = null;
 audio.volume = 1
 
 window.addEventListener('load', () => {
-    document.getElementsByClassName("songinfo")[0].style.display = "none";
+    songinfo.style.display = "none";
 });
 
 audio.addEventListener('waiting', () => {
 slowInternetTimeout = setTimeout(() => {
-    document.getElementsByClassName("songinfo")[0].style.display = "none";
-    //show buffering
-    loadingIcon.innerHTML = '<i class="fa-solid fa-spinner fa-spin-pulse"></i> Stream loading.';
+    playstopBtn.innerHTML = '<i class="fa-solid fa-spinner fa-spin-pulse"></i>';
     });
 });
 
@@ -23,24 +22,28 @@ if(slowInternetTimeout != null){
     slowInternetTimeout = null;
     //continue playing
     loadingIcon.innerHTML = "";
-    document.getElementsByClassName("songinfo")[0].style.display = "block";
+    songinfo.style.display = "block";
+    playstopBtn.innerHTML = '<i class="fa-solid fa-circle-pause" onclick="playstop()"></i>';
     }
 });
 
 audio.addEventListener('ended', () => {
     loadingIcon.innerHTML = '<i class="fa-solid fa-spinner fa-triangle-exclamation"></i> DZCP is now signing off. Thank you for listening.'
-    playstopBtn.src = 'images/play.png';
+    playstopBtn.innerHTML = '<i class="fa-solid fa-circle-play" onclick="playstop()"></i>';
 });
 
 audio.addEventListener('error', () => {
     if(audio.error.code === 4) {
         loadingIcon.innerHTML = '<i class="fa-solid fa-spinner fa-triangle-exclamation"></i> The station is offline. Come back soon.'
+        playstopBtn.innerHTML = '<i class="fa-solid fa-circle-play" onclick="playstop()"></i>';
     } else if (audio.error.code === 3){
         audio.pause();
         loadingIcon.innerHTML = '<i class="fa-solid fa-spinner fa-triangle-exclamation"></i> A decoding error has occurred. Refresh your browser.'
+        playstopBtn.innerHTML = '<i class="fa-solid fa-circle-play" onclick="playstop()"></i>';
     } else if (audio.error.code === 2){
         audio.pause();
         loadingIcon.innerHTML = '<i class="fa-solid fa-spinner fa-triangle-exclamation"></i> A network error has occurred. Please try again.'
+        playstopBtn.innerHTML = '<i class="fa-solid fa-circle-play" onclick="playstop()"></i>';
     }
 })
 
@@ -49,14 +52,14 @@ function playstop() {
         if(audio.paused) {
             audio.load()
             audio.play();
-            playstopBtn.src = 'images/stop.png';
         } else {
             audio.pause();
-            playstopBtn.src = 'images/play.png';
+            loadingIcon.innerHTML = '';
+            playstopBtn.innerHTML = '<i class="fa-solid fa-circle-play" onclick="playstop()"></i>';
         }
     } catch (err) {
         loadingIcon.innerHTML = '<i class="fa-solid fa-spinner fa-triangle-exclamation"></i> An error has occured.'
-        playstopBtn.src = 'images/play.png';
+        playstopBtn.innerHTML = '<i class="fa-solid fa-circle-play" onclick="playstop()"></i>';
     }
 }
 
